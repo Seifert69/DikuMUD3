db77b00eba02910e70806f5d12c66b1cd49d0134
Michael <michael@corleone.com>
2020-07-05 12:57:09 +0200

    formatting

--- vme/src/act_skills.cpp
+++ vme/src/act_skills.cpp
@@ -10,8 +10,7 @@
 #include "skills.h"
 #include "magic.h"
 
-void
-do_backstab (class unit_data * ch, char *arg, const struct command_info *cmd)
+void do_backstab(class unit_data *ch, char *arg, const struct command_info *cmd)
 /*
  * coded: Mon Jun 22 00:22:44 MET DST 1992 [HH]
  * tested: No
@@ -22,79 +21,79 @@ do_backstab (class unit_data * ch, char *arg, const struct command_info *cmd)
     int skilla, skillb, hm;
     char *oarg = arg;
 
-    if (str_is_empty (arg))
+    if (str_is_empty(arg))
     {
-        send_to_char ("Backstab who?<br/>", ch);
+        send_to_char("Backstab who?<br/>", ch);
         return;
     }
 
-    skilla = IS_PC (ch) ? PC_SKI_SKILL (ch, SKI_BACKSTAB) : CHAR_BRA (ch);
+    skilla = IS_PC(ch) ? PC_SKI_SKILL(ch, SKI_BACKSTAB) : CHAR_BRA(ch);
 
     if (skilla == 0)
     {
-        send_to_char ("You must practice first.<br/>", ch);
+        send_to_char("You must practice first.<br/>", ch);
         return;
     }
 
-    if ((vict = find_unit (ch, &arg, 0, FIND_UNIT_SURRO)) == NULL)
+    if ((vict = find_unit(ch, &arg, 0, FIND_UNIT_SURRO)) == NULL)
     {
-        send_to_char ("Noone here by that name.<br/>", ch);
+        send_to_char("Noone here by that name.<br/>", ch);
         return;
     }
 
-    if (!IS_CHAR (vict))
+    if (!IS_CHAR(vict))
     {
-        send_to_char ("But it's dead already.<br/>", ch);
+        send_to_char("But it's dead already.<br/>", ch);
         return;
     }
 
     if (vict == ch)
     {
-        send_to_char ("How can you sneak up on yourself?<br/>", ch);
+        send_to_char("How can you sneak up on yourself?<br/>", ch);
         return;
     }
 
-    if (pk_test (ch, vict, TRUE))
+    if (pk_test(ch, vict, TRUE))
         return;
 
-    if ((stabber = equipment_type (ch, WEAR_WIELD, ITEM_WEAPON)) == NULL)
+    if ((stabber = equipment_type(ch, WEAR_WIELD, ITEM_WEAPON)) == NULL)
     {
-        send_to_char ("You need to wield a weapon, to make it a succes.<br/>",
-                      ch);
-        return;
-    }
-
-    if (OBJ_VALUE (stabber, 0) != WPN_DAGGER &&
-            OBJ_VALUE (stabber, 0) != WPN_SHORT_SWORD &&
-            OBJ_VALUE (stabber, 0) != WPN_WAKIZASHI)
-    {
-        send_to_char ("Only small piercing weapons can be used "
-                      "for backstabbing.<br/>", ch);
+        send_to_char("You need to wield a weapon, to make it a succes.<br/>",
+                     ch);
         return;
     }
 
-    if (CHAR_FIGHTING (vict))
+    if (OBJ_VALUE(stabber, 0) != WPN_DAGGER &&
+        OBJ_VALUE(stabber, 0) != WPN_SHORT_SWORD &&
+        OBJ_VALUE(stabber, 0) != WPN_WAKIZASHI)
     {
-        send_to_char ("You can't backstab a fighting person, too alert!<br/>",
-                      ch);
+        send_to_char("Only small piercing weapons can be used "
+                     "for backstabbing.<br/>",
+                     ch);
         return;
     }
 
-    if (CHAR_AWAKE (vict))
+    if (CHAR_FIGHTING(vict))
     {
-        skillb = IS_PC (vict) ? PC_SKI_SKILL (vict, SKI_BACKSTAB) :
-                 CHAR_BRA (vict);
+        send_to_char("You can't backstab a fighting person, too alert!<br/>",
+                     ch);
+        return;
+    }
+
+    if (CHAR_AWAKE(vict))
+    {
+        skillb = IS_PC(vict) ? PC_SKI_SKILL(vict, SKI_BACKSTAB) : CHAR_BRA(vict);
 
         /* For each recent backstab, victim gets a +50 bonus to resist. */
 
-        if ((paf = affected_by_spell (vict, ID_BACKSTABBED)))
+        if ((paf = affected_by_spell(vict, ID_BACKSTABBED)))
             skillb += 50 * paf->data[0];
 
-        hm = resistance_skill_check (effective_dex (ch), CHAR_DEX (vict),
-                                     skilla, skillb);
+        hm = resistance_skill_check(effective_dex(ch), CHAR_DEX(vict),
+                                    skilla, skillb);
     }
     else
-        hm = resistance_skill_check (effective_dex (ch), 0, skilla, 0);
+        hm = resistance_skill_check(effective_dex(ch), 0, skilla, 0);
 
     if (paf == NULL)
     {
@@ -106,7 +105,7 @@ do_backstab (class unit_data * ch, char *arg, const struct command_info *cmd)
         af.lastf_i = TIF_NONE;
         af.applyf_i = APF_NONE;
         af.data[0] = 1;
-        create_affect (vict, &af);
+        create_affect(vict, &af);
     }
     else
     {
@@ -115,29 +114,26 @@ do_backstab (class unit_data * ch, char *arg, const struct command_info *cmd)
         paf->data[0]++;
     }
 
-
     if (hm < 0)
     {
-        damage (ch, vict, stabber, 0,
-                MSG_TYPE_SKILL, SKI_BACKSTAB, COM_MSG_MISS);
+        damage(ch, vict, stabber, 0,
+               MSG_TYPE_SKILL, SKI_BACKSTAB, COM_MSG_MISS);
     }
     else
     {
         int dam;
         int att_bonus;
 
-        att_bonus = melee_bonus (ch, vict, hit_location (ch, vict),
-                                 NULL, NULL, NULL, NULL);
+        att_bonus = melee_bonus(ch, vict, hit_location(ch, vict),
+                                NULL, NULL, NULL, NULL);
 
-        att_bonus += open100 () + hm;
+        att_bonus += open100() + hm;
 
-        dam = weapon_damage (att_bonus, OBJ_VALUE (stabber, 0), ARM_CLOTHES);
+        dam = weapon_damage(att_bonus, OBJ_VALUE(stabber, 0), ARM_CLOTHES);
 
-        send_done (ch, stabber, vict, dam, cmd, oarg);
+        send_done(ch, stabber, vict, dam, cmd, oarg);
 
-        damage (ch, vict, stabber, dam,
-                MSG_TYPE_SKILL, SKI_BACKSTAB, COM_MSG_EBODY);
+        damage(ch, vict, stabber, dam,
+               MSG_TYPE_SKILL, SKI_BACKSTAB, COM_MSG_EBODY);
     }
 }
-
-
