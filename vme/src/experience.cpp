d15adf23c1019fcb48324e4bbe8eeb5c56acc266
Michael <michael@corleone.com>
2020-07-09 07:02:30 +0200

    Changed dynamics of melee and spell bonus. More dynamic and less level dependant

--- vme/src/experience.cpp
+++ vme/src/experience.cpp
@@ -23,12 +23,11 @@
 #include "common.h"
 #include "fight.h"
 
-
-// MS2020 Trying to decipher the purpose
-//   level 0 - 20 : Changes nothing  
+// MS2020 Trying to decipher the purpose & get rid of this kludge for a more dynamic point system
+//   level 0 - 20 : Changes nothing
 //   up to lvl 50 : add 5% bonus to points for each level above 20, for points up to 100 max.
 //                  will yield bonus from 0..150
-//   above lvl 50 : 
+//   above lvl 50 :
 //                  will yield bonus from 150..150+5/level
 //
 
@@ -49,6 +48,8 @@ int kludge_bonus(int level, int points)
 
    return b;
 }
+
+
 
 /* Let 'def' attempt to use his shield against 'att'. If the shield */
 /* is magic, then it is easier to use. Material & magic does also   */
@@ -116,7 +117,9 @@ int shield_bonus(class unit_data *att, class unit_data *def,
    return def_shield_bonus;
 }
 
-int spell_bonus(class unit_data *att, class unit_data *medium,
+
+
+int dikuii_spell_bonus(class unit_data *att, class unit_data *medium,
                 class unit_data *def,
                 int hit_loc, int spell_number,
                 int *pDef_armour_type, class unit_data **pDef_armour)
@@ -166,7 +169,8 @@ int spell_bonus(class unit_data *att, class unit_data *medium,
    if (CHAR_AWAKE(def))
    {
       hm = (5 * (spell_attack_ability(medium, spell_number) -
-                 spell_ability(def, ABIL_BRA, spell_number))) / 2 +
+                 spell_ability(def, ABIL_BRA, spell_number))) /
+               2 +
            2 * (att_spl_knowledge - def_spl_knowledge) - def_bonus;
    }
    else
@@ -176,13 +180,75 @@ int spell_bonus(class unit_data *att, class unit_data *medium,
    return MAX(-50, hm);
 }
 
+
+
+int spell_bonus(class unit_data *att, class unit_data *medium,
+                class unit_data *def,
+                int hit_loc, int spell_number,
+                int *pDef_armour_type, class unit_data **pDef_armour)
+{
+   int att_spl_knowledge, def_spl_knowledge;
+   int att_bonus;
+   int def_bonus;
+   int def_armour_type;
+   class unit_data *def_armour;
+   int hm;
+
+   att_bonus = CHAR_OFFENSIVE(att);
+   def_bonus = CHAR_DEFENSIVE(def);
+
+   /* If attacker can't see the defender, then the defender has a   */
+   /* much better effective attack (since attacker can't see him    */
+   if (!CHAR_CAN_SEE(att, def))
+      att_bonus -= 12;
+
+   if ((UNIT_IS_GOOD(att) && affected_by_spell(def, ID_PROT_GOOD)) ||
+       (UNIT_IS_EVIL(att) && affected_by_spell(def, ID_PROT_EVIL)))
+      def_bonus += 10;
+
+   if ((def_armour = equipment_type(def, hit_loc, ITEM_ARMOR)))
+   {
+      def_armour_type = OBJ_VALUE(def_armour, 0);
+      if (is_in(OBJ_VALUE(def_armour, 1), -25, 25))
+         def_bonus += OBJ_VALUE(def_armour, 1) + OBJ_VALUE(def_armour, 2);
+      else
+         slog(LOG_ALL, 0, "Illegal armour bonus.");
+   }
+   else
+      def_armour_type = CHAR_NATURAL_ARMOUR(def);
+
+   if (pDef_armour_type)
+      *pDef_armour_type = def_armour_type;
+
+   if (pDef_armour)
+      *pDef_armour = def_armour;
+
+   att_spl_knowledge = spell_attack_skill(medium, spell_number);
+   def_spl_knowledge = spell_defense_skill(def, spell_number);
+
+   if (CHAR_AWAKE(def))
+   {
+      hm = 2 * (spell_attack_ability(medium, spell_number) - spell_ability(def, ABIL_BRA, spell_number) +
+               (att_spl_knowledge - def_spl_knowledge/3) + (att_bonus - def_bonus));
+   }
+   else
+   {
+      hm = 2 * (spell_attack_ability(medium, spell_number) +
+               (att_spl_knowledge) + (att_bonus - def_bonus));
+   }
+
+   return MAX(-50, hm);
+}
+
+
+
 /* If 'att' hits 'def' on 'hit_loc' then what is his basic attack */
 /* modification? This value should then be added to an open roll  */
 /* and looked up upon the damage_charts. If armour type points    */
 /* to anything, then it will be set to the defenders armour_type  */
 /* which should be used upon lookup                               */
 
-int melee_bonus(class unit_data *att, class unit_data *def,
+int dikuii_melee_bonus(class unit_data *att, class unit_data *def,
                 int hit_loc,
                 int *pAtt_weapon_type, class unit_data **pAtt_weapon,
                 int *pDef_armour_type, class unit_data **pDef_armour,
@@ -307,6 +373,335 @@ int melee_bonus(class unit_data *att, class unit_data *def,
    return MAX(-50, hm);
 }
 
+
+
+/* If 'att' hits 'def' on 'hit_loc' then what is his basic attack */
+/* modification? This value should then be added to an open roll  */
+/* and looked up upon the damage_charts. If armour type points    */
+/* to anything, then it will be set to the defenders armour_type  */
+/* which should be used upon lookup                               */
+
+int melee_bonus(class unit_data *att, class unit_data *def,
+                int hit_loc,
+                int *pAtt_weapon_type, class unit_data **pAtt_weapon,
+                int *pDef_armour_type, class unit_data **pDef_armour,
+                int primary)
+{
+   int att_bonus, att_wpn_knowledge;
+   int def_dex, def_bonus, def_wpn_knowledge;
+
+   class unit_data *att_wpn;
+   int att_wpn_type;
+
+   int def_armour_type;
+   class unit_data *def_armour;
+
+   int hm;
+
+   def_dex = effective_dex(def);
+
+   att_bonus = CHAR_OFFENSIVE(att);
+   def_bonus = CHAR_DEFENSIVE(def);
+
+   if (pAtt_weapon_type &&
+       is_in(*pAtt_weapon_type, WPN_GROUP_MAX, WPN_TREE_MAX - 1))
+   {
+      att_wpn = NULL;
+      att_wpn_type = *pAtt_weapon_type;
+      att_wpn_knowledge = weapon_attack_skill(att, att_wpn_type);
+   }
+   else
+   {
+      if (primary)
+         att_wpn = equipment_type(att, WEAR_WIELD, ITEM_WEAPON);
+      else
+         att_wpn = equipment_type(att, WEAR_HOLD, ITEM_WEAPON);
+
+      if (att_wpn)
+      {
+         att_wpn_type = OBJ_VALUE(att_wpn, 0); /* [0] is category */
+         att_wpn_knowledge = weapon_attack_skill(att, att_wpn_type);
+         if (is_in(OBJ_VALUE(att_wpn, 1), -25, 25))
+            att_bonus += OBJ_VALUE(att_wpn, 1) + OBJ_VALUE(att_wpn, 2);
+      }
+      else
+      {
+         att_wpn_type = CHAR_ATTACK_TYPE(att);
+         att_wpn_knowledge = weapon_attack_skill(att, att_wpn_type);
+      }
+   }
+
+   if (char_dual_wield(att))
+   {
+      int dual_skill;
+
+      if (IS_PC(att))
+         dual_skill = PC_SKI_SKILL(att, SKI_DUAL_WIELD);
+      else
+         dual_skill = CHAR_DEX(att);
+
+      if (primary)
+         att_bonus -= MAX(0, 25 - (dual_skill / 4));
+      else
+         att_bonus -= MAX(0, 50 - (dual_skill / 4));
+   }
+
+   def_wpn_knowledge = weapon_defense_skill(def, att_wpn_type);
+
+   if (CHAR_FIGHTING(def) != att)
+      def_bonus -= 25;
+
+   /* If attacker can't see the defender, then the defender has a   */
+   /* much better effective dexterity (since attacker can't see him */
+   if (!CHAR_CAN_SEE(att, def))
+      def_bonus += 25;
+
+   if (!CHAR_CAN_SEE(def, att))
+      att_bonus += 25;
+
+   /* Slaying Weapons */
+   if (att_wpn && OBJ_VALUE(att_wpn, 3) == CHAR_RACE(def))
+      att_bonus += 25;
+
+   if ((UNIT_IS_GOOD(att) && affected_by_spell(def, ID_PROT_GOOD)) ||
+       (UNIT_IS_EVIL(att) && affected_by_spell(def, ID_PROT_EVIL)))
+      def_bonus += 20;
+
+   if ((def_armour = equipment_type(def, hit_loc, ITEM_ARMOR)))
+   {
+      def_armour_type = OBJ_VALUE(def_armour, 0);
+      if (is_in(OBJ_VALUE(def_armour, 1), -25, 25))
+         def_bonus += OBJ_VALUE(def_armour, 1) + OBJ_VALUE(def_armour, 2);
+      else
+         slog(LOG_ALL, 0, "Illegal armour bonus.");
+   }
+   else
+      def_armour_type = CHAR_NATURAL_ARMOUR(def);
+
+   if (pAtt_weapon_type)
+      *pAtt_weapon_type = att_wpn_type;
+
+   if (pAtt_weapon)
+      *pAtt_weapon = att_wpn;
+
+   if (pDef_armour_type)
+      *pDef_armour_type = def_armour_type;
+
+   if (pDef_armour)
+      *pDef_armour = def_armour;
+
+   int att_abil = weapon_attack_ability(att, att_wpn_type);
+
+   if (CHAR_AWAKE(def))
+      hm = 2* ((att_abil - def_dex) + (att_wpn_knowledge - def_wpn_knowledge/3) + att_bonus - def_bonus);
+   else
+      hm = 2* (att_abil + att_wpn_knowledge + att_bonus);
+
+   return MAX(-50, hm);
+}
+
+
+// Appends an explanation of melee_bonus to the string str
+//
+void stat_melee_bonus(string &str,
+                      class unit_data *att, class unit_data *def,
+                      int hit_loc,
+                      int *pAtt_weapon_type, class unit_data **pAtt_weapon,
+                      int *pDef_armour_type, class unit_data **pDef_armour,
+                      int primary)
+{
+   int att_bonus, att_wpn_knowledge;
+   int def_dex, def_bonus, def_wpn_knowledge;
+   char buf[MAX_STRING_LENGTH];
+
+   class unit_data *att_wpn;
+   int att_wpn_type;
+
+   int def_armour_type;
+   class unit_data *def_armour;
+
+   int hm, tmp;
+
+   str.append("<u>Melee bonus explanation</u>:<br/><pre>");
+   sprintf(buf, "                        ATT     DEF<br/>");
+   str.append(buf);
+
+   att_bonus = CHAR_OFFENSIVE(att);
+   def_bonus = CHAR_DEFENSIVE(def);
+   sprintf(buf, "Off / Def bonus     :  %4d   %4d<br/>", att_bonus, def_bonus);
+   str.append(buf);
+
+   tmp = 0;
+
+   if (primary)
+      att_wpn = equipment_type(att, WEAR_WIELD, ITEM_WEAPON);
+   else
+      att_wpn = equipment_type(att, WEAR_HOLD, ITEM_WEAPON);
+
+   if (att_wpn)
+   {
+      att_wpn_type = OBJ_VALUE(att_wpn, 0); /* [0] is category ??? */
+      att_wpn_knowledge = weapon_attack_skill(att, att_wpn_type);
+      if (is_in(OBJ_VALUE(att_wpn, 1), -25, 25))
+      {
+         tmp = OBJ_VALUE(att_wpn, 1) + OBJ_VALUE(att_wpn, 2);
+         att_bonus += tmp;
+      }
+   }
+   else
+   {
+      att_wpn_type = CHAR_ATTACK_TYPE(att);
+      att_wpn_knowledge = weapon_attack_skill(att, att_wpn_type);
+   }
+
+   def_wpn_knowledge = weapon_defense_skill(def, att_wpn_type);
+   sprintf(buf, "Weapon skill        :  %4d   %4d<br/>", att_wpn_knowledge, def_wpn_knowledge);
+   str.append(buf);
+
+   sprintf(buf, "Att Weapon mat+mag  :  %4d       <br/>", tmp);
+   str.append(buf);
+
+   tmp = 0;
+   if (char_dual_wield(att))
+   {
+      int dual_skill;
+
+      if (IS_PC(att))
+         dual_skill = PC_SKI_SKILL(att, SKI_DUAL_WIELD);
+      else
+         dual_skill = CHAR_DEX(att);
+
+      if (primary)
+      {
+         tmp = -MAX(0, 25 - (dual_skill / 4));
+         att_bonus += tmp;
+      }
+      else
+      {
+         tmp = -MAX(0, 50 - (dual_skill / 4));
+         att_bonus += tmp;
+      }
+   }
+   sprintf(buf, "Dual wield          :  %4d       <br/>", tmp);
+   str.append(buf);
+
+   tmp = 0;
+   if (CHAR_FIGHTING(def) != att)
+   {
+      tmp = -25;
+      def_bonus += tmp;
+   }
+
+   sprintf(buf, "Fighting attacker?  :  %4d       <br/>", tmp);
+   str.append(buf);
+
+   /* If attacker can't see the defender, then the defender has a   */
+   /* much better effective dexterity (since attacker can't see him */
+   if (!CHAR_CAN_SEE(att, def))
+   {
+      def_bonus += 25;
+      sprintf(buf, "Attacker cant see    :       %4d  <br/>", 25);
+      str.append(buf);
+   }
+
+   if (!CHAR_CAN_SEE(def, att))
+   {
+      sprintf(buf, "Defender cant see    :  %4d  <br/>", 25);
+      str.append(buf);
+      att_bonus += 25;
+   }
+
+   /* Slaying Weapons */
+   if (att_wpn && OBJ_VALUE(att_wpn, 3) == CHAR_RACE(def))
+   {
+      sprintf(buf, "Slaying              :  %4d  <br/>", 50);
+      str.append(buf);
+      att_bonus += 50;
+   }
+
+   if ((UNIT_IS_GOOD(att) && affected_by_spell(def, ID_PROT_GOOD)) ||
+       (UNIT_IS_EVIL(att) && affected_by_spell(def, ID_PROT_EVIL)))
+   {
+      sprintf(buf, "Evil good protect     :       %4d  <br/>", 20);
+      str.append(buf);
+      def_bonus += 20;
+   }
+
+   if ((def_armour = equipment_type(def, hit_loc, ITEM_ARMOR)))
+   {
+      def_armour_type = OBJ_VALUE(def_armour, 0);
+      if (is_in(OBJ_VALUE(def_armour, 1), -25, 25))
+      {
+         tmp = OBJ_VALUE(def_armour, 1) + OBJ_VALUE(def_armour, 2);
+         def_bonus += tmp;
+         sprintf(buf, "Armor bonus         :       %4d  <br/>", tmp);
+         str.append(buf);
+      }
+      else
+         slog(LOG_ALL, 0, "Illegal armour bonus.");
+   }
+   else
+      def_armour_type = CHAR_NATURAL_ARMOUR(def);
+
+   if (pAtt_weapon_type)
+      *pAtt_weapon_type = att_wpn_type;
+
+   if (pAtt_weapon)
+      *pAtt_weapon = att_wpn;
+
+   if (pDef_armour_type)
+      *pDef_armour_type = def_armour_type;
+
+   if (pDef_armour)
+      *pDef_armour = def_armour;
+
+   /*tmp = kludge_bonus(CHAR_LEVEL(att), att_wpn_knowledge);
+   att_wpn_knowledge += tmp;
+   tmp2 = kludge_bonus(CHAR_LEVEL(def), def_wpn_knowledge) / 2;
+   def_wpn_knowledge += tmp2;
+   sprintf(buf, "Kludge bonus        :  %4d   %4d<br/>", tmp, tmp2);
+   str.append(buf);*/
+
+   int att_abil = weapon_attack_ability(att, att_wpn_type);
+   sprintf(buf, "Abil for weapon     :  %4d<br/>", att_abil);
+   str.append(buf);
+
+   def_dex = effective_dex(def);
+   sprintf(buf, "Abil DEX DB         :         %4d<br/>", def_dex);
+   str.append(buf);
+
+
+   if (CHAR_AWAKE(def))
+   {
+      hm = 2 * (att_abil - def_dex + att_wpn_knowledge - def_wpn_knowledge/3 + att_bonus - def_bonus);
+      sprintf(buf, "Result              :  %4d<br/>", hm);
+      str.append(buf);
+   }
+   else
+   {
+      hm = 2 * (att_abil + att_wpn_knowledge + att_bonus);
+      sprintf(buf, "Result (sleep def)  :  %4d<br/>", hm);
+      str.append(buf);
+   }
+   sprintf(buf, "Result = 2*(att_abil-def_abil + att_wpn - def_wpn + att_bonus - def_bonus) + roll<br/>");
+   str.append(buf);
+
+   str.append("<br/>");
+
+   int dam5  = weapon_damage(5 + hm, att_wpn_type, def_armour_type);
+   int dam50 = weapon_damage(50 + hm, att_wpn_type, def_armour_type);
+   int dam95 = weapon_damage(95 + hm, att_wpn_type, def_armour_type);
+
+   sprintf(buf, "Weapon dmg (5/50/95) : %4d %4d %4d<br/>", dam5, dam50, dam95);
+   str.append(buf);
+
+   sprintf(buf, "Rounds to kill def = %d<br/>", UNIT_MAX_HIT(def)/MAX(1, (dam5 + dam50 + dam95) / 3));
+   str.append(buf);
+
+
+   str.append("</pre>");
+}
+
 int base_melee(class unit_data *att, class unit_data *def, int hit_loc)
 {
    int ocp, bonus;
@@ -409,10 +804,10 @@ void do_consider(class unit_data *ch, char *arg, const struct command_info *cmd)
 
    if (IS_IMMORTAL(ch))
    {
-      act("Rounds to be killed by $3n = $2d.", A_SOMEONE, ch, rtd, vict, TO_CHAR);
+      act("Rounds to be killed by $3n = $2d.", A_SOMEONE, ch, &rtd, vict, TO_CHAR);
 
       rtd = base_consider(ch, vict);
-      act("Rounds to kill $3n = $2d.", A_SOMEONE, ch, rtd, vict, TO_CHAR);
+      act("Rounds to kill $3n = $2d.", A_SOMEONE, ch, &rtd, vict, TO_CHAR);
       return;
    }
 
