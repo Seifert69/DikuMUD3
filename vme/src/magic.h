8ef08a098511ab93e9125e287c82d2d5be3124bd
Michael <michael@corleone.com>
2020-09-03 09:46:29 +0200

    formatting

--- vme/src/magic.h
+++ vme/src/magic.h
@@ -5,7 +5,6 @@
  $Revision: 2.3 $
  */
 
-
 #include "structs.h"
 #include "utils.h"
 #include "skills.h"
@@ -22,36 +21,34 @@
 #include "common.h"
 #include "fight.h"
 
-
 #ifndef _MUD_MAGIC_H
 #define _MUD_MAGIC_H
 
 /* #define MAGIC_POWER(ch)  \
    MAX(CHAR_MAG(ch), CHAR_DIV(ch)) */
 
-int dil_effect (char *pStr, struct spell_args *sa);
-int object_power (class unit_data * unit);
-ubit1 use_mana (class unit_data * medium, int mana);
-ubit1 cast_magic_now (class unit_data * ch, int mana);
+int dil_effect(char *pStr, struct spell_args *sa);
+int object_power(class unit_data *unit);
+ubit1 use_mana(class unit_data *medium, int mana);
+ubit1 cast_magic_now(class unit_data *ch, int mana);
 
-ubit1 may_teleport_away (class unit_data * unit);
-ubit1 may_teleport_to (class unit_data * unit, class unit_data * dest);
-ubit1 may_teleport (class unit_data * unit, class unit_data * dest);
+ubit1 may_teleport_away(class unit_data *unit);
+ubit1 may_teleport_to(class unit_data *unit, class unit_data *dest);
+ubit1 may_teleport(class unit_data *unit, class unit_data *dest);
 
+int spell_cast_check(class unit_data *att, int spell);
+int spell_resistance(class unit_data *att, class unit_data *def,
+                     int spell);
+int spell_ability(class unit_data *u, int ability, int spell);
+int spell_attack_ability(class unit_data *medium, int spell);
+int spell_attack_skill(class unit_data *unit, int spell);
+int spell_defense_skill(class unit_data *unit, int spell);
 
-int spell_cast_check (class unit_data * att, int spell);
-int spell_resistance (class unit_data * att, class unit_data * def,
-                      int spell);
-int spell_ability (class unit_data * u, int ability, int spell);
-int spell_attack_ability (class unit_data * medium, int spell);
-int spell_attack_skill (class unit_data * unit, int spell);
-int spell_defense_skill (class unit_data * unit, int spell);
+int spell_offensive(struct spell_args *sa, int spell_number, int bonus = 0);
 
-int spell_offensive (struct spell_args *sa, int spell_number, int bonus = 0);
-
-int variation (int n, int d, int u);
+int variation(int n, int d, int u);
 /*last two internal spells*/
-void spell_clear_skies (struct spell_args *sa);
-void spell_storm_call (struct spell_args *sa);
+void spell_clear_skies(struct spell_args *sa);
+void spell_storm_call(struct spell_args *sa);
 
 #endif /* _MUD_MAGIC_H */
