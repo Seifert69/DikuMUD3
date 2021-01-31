43732e12608b5b6caf316c328e68e2a5863bf8ea
seifert69 <michael@corleone.com>
2021-01-31 14:10:01 -0800

    updared to work on TEST mud

--- vme/bin/allow/compile.sh
+++ vme/bin/allow/compile.sh
@@ -4,16 +4,30 @@
 # running from DIL. Symbolic links and scripts are your friends here
 # E.g. ln -s /usr/bin/gh
 
-umask 666
+umask 000
 
-VMEPATH=/home/seifert/temp/diku/DikuMUD3/vme
-LOGNAME=$VMEPATH/bin/allow/compile.log
+HOMEPATH=/home/mud
+BUILDPATH=/home/mud/build
+VMEPATH=$HOMEPATH/vme2.0/
+LONGLOGNAME=$VMEPATH/bin/allow/compile.log
 LOGFILE=$VMEPATH/lib/dilfiles/strings/compile.log
 
+function myexec {
+    eval $@
+    if [ $? -ne 0 ]; then
+        echo "ERROR running " $@ >> $LOGFILE
+    fi
+    return
+}
+
 rm -f $LOGFILE
-rm -f $LOGNAME
-echo `/bin/date` 2>&1 >> $LOGNAME
-chmod 666 $LOGNAME
-sleep 1
-mv -v $LOGNAME $LOGFILE
-chmod 666 $LOGFILE
+rm -f $LONGLOGNAME
+echo `/bin/date` 2>&1 >> $LONGLOGNAME
+echo `/bin/date` 2>&1 >> $LOGFILE
+myexec cd $HOMEPATH/build/
+myexec $HOMEPATH/build/buildit.sh 2>&1 >> $LONGLOGNAME
+myexec $HOMEPATH/build/totest.sh 2>&1 >> $LONGLOGNAME
+cd $VMEPATH/bin/allow/
+
+echo "*** DONE ***" `date` 2>&1 >> $LONGLOGNAME
+echo "*** DONE ***" `date` 2>&1 >> $LOGFILE
