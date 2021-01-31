040688db1f7ead4e37363b0c9825f0cf952c33a5
seifert69 <michael@corleone.com>
2021-01-31 09:13:27 -0800

    adding absolute path

--- vme/bin/allow/compile.sh
+++ vme/bin/allow/compile.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 # Place any executable file in this directory that you want to allow
 # running from DIL. Symbolic links and scripts are your friends here
@@ -6,12 +6,13 @@
 
 umask 666
 
-LOGNAME=compile.log
-LOGFILE=../../lib/dilfiles/strings/$LOGNAME
+VMEPATH=/home/seifert/temp/diku/DikuMUD3/vme
+LOGNAME=$VMEPATH/bin/allow/compile.log
+LOGFILE=$VMEPATH/lib/dilfiles/strings/compile.log
 
 rm -f $LOGFILE
 rm -f $LOGNAME
-echo `/usr/bin/date` 2>&1 >> $LOGNAME
+echo `/bin/date` 2>&1 >> $LOGNAME
 chmod 666 $LOGNAME
 sleep 1
 mv -v $LOGNAME $LOGFILE
