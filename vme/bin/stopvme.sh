18e8383589b55ac9de2c2abab22293271b6402fe
Michael Seifert <michael@corleone.com>
2022-01-10 09:52:35 -0800

    improving server run scripts

--- vme/bin/stopvme.sh
+++ vme/bin/stopvme.sh
@@ -15,26 +15,32 @@ if [ -z ${VME_ROOT} ]; then
    VME_ROOT="$PWD/.."
 fi
 
+echo "zapping all scripts and binaries running the VME and company."
+
 #
 # Stop the scripts
 #
 PIDS="$(pgrep -x runvme.sh -u $USER)"
 if [ ! -z "${PIDS}" ]; then
+   echo "killing runvme.sh"
    kill -9 $PIDS
 fi
 
 PIDS="$(pgrep -x runmplex.sh -u $USER)"
 if [ ! -z "${PIDS}" ]; then
+   echo "killing runmplex.sh"
    kill -9 $PIDS
 fi
 
 PIDS="$(pgrep -x rundispatcher.sh -u $USER)"
 if [ ! -z "${PIDS}" ]; then
+   echo "killing rundispatcher.sh"
    kill -9 $PIDS
 fi
 
 PIDS="$(pgrep -x runmuddiscord.sh -u $USER)"
 if [ ! -z "${PIDS}" ]; then
+   echo "killing runmuddiscord.sh"
    kill -9 $PIDS
 fi
 
@@ -44,21 +50,35 @@ fi
 PIDS="$(pgrep -x vme -u $USER)"
 #echo "vme: $PIDS"
 if [ ! -z "${PIDS}" ]; then
+   echo "killing vme"
    kill -SIGINT $PIDS
 fi
 
 
 PIDS="$(pgrep -x mplex -u $USER)"
 if [ ! -z "${PIDS}" ]; then
+   echo "killing mplex"
    kill -SIGQUIT $PIDS
 fi
 
 PIDS="$(pgrep -x "python3 -u muddiscord.py" -u $USER)"
 if [ ! -z "${PIDS}" ]; then
+   echo "killing muddiscord.py"
    kill -9 $PIDS
 fi
 
 PIDS="$(pgrep -x "python3 -u dispatcher.py" -u $USER)"
 if [ ! -z "${PIDS}" ]; then
+   echo "killing dispatcher.py"
+   kill -9 $PIDS
+fi
+
+echo "sleeping 5 seconds and making sure no VME processes are still running"
+sleep 5
+
+PIDS="$(pgrep -x vme -u $USER)"
+#echo "vme: $PIDS"
+if [ ! -z "${PIDS}" ]; then
+   echo "this shouldn't happen, killing vme again."
    kill -9 $PIDS
 fi
