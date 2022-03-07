5ef7b98fcc4d079611f9bc34e6f352b834bd91be
Michael Seifert <michael@corleone.com>
2022-03-07 12:00:33 -0800

    test

--- vme/src/teach.cpp
+++ vme/src/teach.cpp
@@ -1808,7 +1808,7 @@ int teach_init(spec_arg *sarg)
     sarg->fptr->setHeartBeat(PULSE_SEC);
 
     assert(sarg->fptr->getFunctionPointerIndex() != SFUN_DIL_INTERNAL);
-    free(sarg->fptr->getData()); /* Free the text string! */
+    // TEST XXX free(sarg->fptr->getData()); /* Free the text string! */
     sarg->fptr->setData(packet);
 
     /* Call teaching in case initialization occurs with first command! */
