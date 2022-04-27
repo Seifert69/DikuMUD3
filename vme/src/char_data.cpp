a21b01270bf920257d7305e9e331198ad28ce44e
Adrian Cornish <github@bluedreamer.com>
2022-04-27 10:20:12 -0600

    Extracted char_data into its own source files

---
+++ vme/src/char_data.cpp
@@ -0,0 +1,545 @@
+#include "char_data.h"
+
+#include "structs.h"
+
+char_data::char_data(ubit8 unit_type)
+    : unit_data(unit_type)
+    , m_descriptor{nullptr}
+    , m_combat{nullptr}
+    , m_master{nullptr}
+    , m_last_room{nullptr}
+    , m_points{}
+    , m_followers{nullptr}
+    , m_last_attacker{nullptr}
+    , m_money{nullptr}
+    , m_last_attacker_type{0}
+{
+    g_world_nochars++;
+
+    // MS2020 memset(&points, 0, sizeof (points));
+    // Removed because class constructor already initialize class variables.
+}
+
+char_data::~char_data()
+{
+#ifdef DMSERVER
+    if (m_money)
+    {
+        FREE(m_money);
+    }
+    if (m_last_attacker)
+    {
+        FREE(m_last_attacker);
+    }
+#endif
+    g_world_nochars--;
+}
+
+const descriptor_data *char_data::getDescriptor() const
+{
+    return m_descriptor;
+}
+
+descriptor_data *char_data::getDescriptor()
+{
+    return m_descriptor;
+}
+
+void char_data::setDescriptor(descriptor_data *value)
+{
+    m_descriptor = value;
+}
+
+const cCombat *char_data::getCombat() const
+{
+    return m_combat;
+}
+
+cCombat *char_data::getCombat()
+{
+    return m_combat;
+}
+
+void char_data::setCombat(cCombat *value)
+{
+    m_combat = value;
+}
+
+const unit_data *char_data::getMaster() const
+{
+    return m_master;
+}
+
+unit_data *char_data::getMaster()
+{
+    return m_master;
+}
+
+unit_data **char_data::getMasterPtr()
+{
+    return &m_master;
+}
+
+void char_data::setMaster(unit_data *value)
+{
+    m_master = value;
+}
+
+const unit_data *char_data::getLastLocation() const
+{
+    return m_last_room;
+}
+
+unit_data *char_data::getLastLocation()
+{
+    return m_last_room;
+}
+
+unit_data **char_data::getLastLocationPtr()
+{
+    return &m_last_room;
+}
+
+void char_data::setLastLocation(unit_data *value)
+{
+    m_last_room = value;
+}
+
+void char_data::readFrom(CByteBuffer &buf, ubit8 unit_version, unit_data *unit, int &error)
+{
+    return m_points.readFrom(buf, unit_version, unit, error);
+}
+
+ubit32 *char_data::getCharacterFlagsPtr()
+{
+    return m_points.getCharacterFlagsPtr();
+}
+
+ubit32 char_data::getCharacterFlags() const
+{
+    return m_points.getCharacterFlags();
+}
+
+void char_data::setAllCharacterFlags(ubit32 value)
+{
+    m_points.setAllCharacterFlags(value);
+}
+
+void char_data::setCharacterFlag(ubit32 value)
+{
+    m_points.setCharacterFlag(value);
+}
+
+void char_data::removeCharacterFlag(ubit32 value)
+{
+    m_points.removeCharacterFlag(value);
+}
+
+void char_data::toggleCharacterFlag(ubit32 value)
+{
+    m_points.toggleCharacterFlag(value);
+}
+
+sbit32 char_data::getPlayerExperience() const
+{
+    return m_points.getPlayerExperience();
+}
+
+sbit32 *char_data::getPlayerExperiencePtr()
+{
+    return m_points.getPlayerExperiencePtr();
+}
+
+void char_data::setPlayerExperience(sbit32 value)
+{
+    m_points.setPlayerExperience(value);
+}
+
+void char_data::increasePlayerExperienceBy(sbit32 value)
+{
+    m_points.increasePlayerExperienceBy(value);
+}
+
+ubit16 char_data::getRace() const
+{
+    return m_points.getRace();
+}
+
+ubit16 *char_data::getRacePtr()
+{
+    return m_points.getRacePtr();
+}
+
+void char_data::setRace(ubit16 value)
+{
+    m_points.setRace(value);
+}
+
+sbit16 char_data::getMana() const
+{
+    return m_points.getMana();
+}
+
+sbit16 *char_data::getManaPtr()
+{
+    return m_points.getManaPtr();
+}
+
+void char_data::setMana(sbit16 value)
+{
+    m_points.setMana(value);
+}
+
+void char_data::decreaseManaBy(sbit16 value)
+{
+    m_points.decreaseManaBy(value);
+}
+
+sbit16 char_data::getEndurance() const
+{
+    return m_points.getEndurance();
+}
+
+sbit16 *char_data::getEndurancePtr()
+{
+    return m_points.getEndurancePtr();
+}
+
+void char_data::setEndurance(sbit16 value)
+{
+    m_points.setEndurance(value);
+}
+
+void char_data::decreaseEnduranceBy(sbit16 value)
+{
+    m_points.decreaseEnduranceBy(value);
+}
+
+sbit16 char_data::getOffensiveBonus() const
+{
+    return m_points.getOffensiveBonus();
+}
+
+void char_data::setOffensiveBonus(sbit16 value)
+{
+    m_points.setOffensiveBonus(value);
+}
+
+sbit16 *char_data::getOffensiveBonusPtr()
+{
+    return m_points.getOffensiveBonusPtr();
+}
+
+sbit16 char_data::getDefensiveBonus() const
+{
+    return m_points.getDefensiveBonus();
+}
+
+void char_data::setDefensiveBonus(sbit16 value)
+{
+    m_points.setDefensiveBonus(value);
+}
+
+sbit16 *char_data::getDefensiveBonusPtr()
+{
+    return m_points.getDefensiveBonusPtr();
+}
+
+int char_data::getNumberOfMeleeAttacks(int naturalAttacks)
+{
+    return std::max(1, (naturalAttacks * getSpeedPercentage()) / 100);
+}
+
+sbit16 char_data::getSpeed() const
+{
+    return m_points.getSpeed();
+}
+
+void char_data::setSpeed(sbit16 value)
+{
+    m_points.setSpeed(value);
+}
+
+void char_data::modifySpeedBy(sbit16 delta)
+{
+    m_points.modifySpeedBy(delta);
+}
+
+int char_data::getSpeedPercentage()
+{
+    sbit16 s = m_points.getSpeed();
+
+    if (s < SPEED_MIN)
+    {
+        s = SPEED_MIN;
+    }
+
+    if (s == SPEED_DEFAULT)
+    {
+        return 100;
+    }
+
+    if (getUnitType() == UNIT_ST_PC)
+    {
+        return std::max(std::min(200, (100 * SPEED_DEFAULT) / s), 25);
+    }
+    return std::max(std::min(300, (100 * SPEED_DEFAULT) / s), 25);
+}
+
+ubit8 char_data::getNaturalArmor() const
+{
+    return m_points.getNaturalArmor();
+}
+
+void char_data::setNaturalArmor(ubit8 value)
+{
+    m_points.setNaturalArmor(value);
+}
+
+ubit8 *char_data::getNaturalArmorPtr()
+{
+    return m_points.getNaturalArmorPtr();
+}
+
+ubit8 char_data::getAttackType() const
+{
+    return m_points.getAttackType();
+}
+
+void char_data::setAttackType(ubit8 value)
+{
+    m_points.setAttackType(value);
+}
+
+ubit8 *char_data::getAttackTypePtr()
+{
+    return m_points.getAttackTypePtr();
+}
+
+ubit8 char_data::getSex() const
+{
+    return m_points.getSex();
+}
+
+void char_data::setSex(ubit8 value)
+{
+    m_points.setSex(value);
+}
+
+ubit8 *char_data::getSexPtr()
+{
+    return m_points.getSexPtr();
+}
+
+ubit8 char_data::getLevel() const
+{
+    return m_points.getLevel();
+}
+
+void char_data::setLevel(ubit8 value)
+{
+    m_points.setLevel(value);
+}
+
+ubit8 *char_data::getLevelPtr()
+{
+    return m_points.getLevelPtr();
+}
+
+void char_data::incrementLevel()
+{
+    m_points.incrementLevel();
+}
+
+ubit8 char_data::getPosition() const
+{
+    return m_points.getPosition();
+}
+
+void char_data::setPosition(ubit8 value)
+{
+    m_points.setPosition(value);
+}
+
+ubit8 *char_data::getPositionPtr()
+{
+    return m_points.getPositionPtr();
+}
+
+sbit16 char_data::getAbilityAtIndex(size_t index) const
+{
+    return m_points.getAbilityAtIndex(index);
+}
+
+sbit16 *char_data::getAbilityAtIndexPtr(size_t index)
+{
+    return m_points.getAbilityAtIndexPtr(index);
+}
+
+sbit16 char_data::getSTR() const
+{
+    return m_points.getSTR();
+}
+
+sbit16 char_data::getDEX() const
+{
+    return m_points.getDEX();
+}
+
+sbit16 char_data::getCON() const
+{
+    return m_points.getCON();
+}
+
+sbit16 char_data::getCHA() const
+{
+    return m_points.getCHA();
+}
+
+sbit16 char_data::getBRA() const
+{
+    return m_points.getBRA();
+}
+
+sbit16 char_data::getMAG() const
+{
+    return m_points.getMAG();
+}
+
+sbit16 char_data::getDIV() const
+{
+    return m_points.getDIV();
+}
+
+sbit16 char_data::getHPP() const
+{
+    return m_points.getHPP();
+}
+
+void char_data::setSTR(sbit16 value)
+{
+    m_points.setSTR(value);
+}
+
+void char_data::setDEX(sbit16 value)
+{
+    m_points.setDEX(value);
+}
+
+void char_data::setCON(sbit16 value)
+{
+    m_points.setCON(value);
+}
+
+void char_data::setCHA(sbit16 value)
+{
+    m_points.setCHA(value);
+}
+
+void char_data::setBRA(sbit16 value)
+{
+    m_points.setBRA(value);
+}
+
+void char_data::setMAG(sbit16 value)
+{
+    m_points.setMAG(value);
+}
+
+void char_data::setDIV(sbit16 value)
+{
+    m_points.setDIV(value);
+}
+
+void char_data::setHPP(sbit16 value)
+{
+    m_points.setHPP(value);
+}
+
+void char_data::setAbilityAtIndexTo(size_t index, sbit16 value)
+{
+    m_points.setAbilityAtIndexTo(index, value);
+}
+
+void char_data::increaseAbilityAtIndexBy(size_t index, sbit16 value)
+{
+    m_points.increaseAbilityAtIndexBy(index, value);
+}
+
+void char_data::decreaseAbilityAtIndexBy(size_t index, sbit16 value)
+{
+    m_points.decreaseAbilityAtIndexBy(index, value);
+}
+
+std::array<sbit16, ABIL_TREE_MAX> &char_data::getAbilitiesArray()
+{
+    return m_points.getAbilitiesArray();
+}
+
+const char_follow_type *char_data::getFollowers() const
+{
+    return m_followers;
+}
+
+char_follow_type *char_data::getFollowers()
+{
+    return m_followers;
+}
+
+void char_data::setFollowers(char_follow_type *value)
+{
+    m_followers = value;
+}
+
+const char *char_data::getLastAttacker() const
+{
+    return m_last_attacker;
+}
+
+void char_data::freeLastAttacker()
+{
+    if (m_last_attacker)
+    {
+        FREE(m_last_attacker);
+    }
+}
+
+void char_data::setLastAttacker(char *value)
+{
+    m_last_attacker = value;
+}
+
+const char *char_data::getMoney() const
+{
+    return m_money;
+}
+
+char *char_data::getMoney()
+{
+    return m_money;
+}
+
+char **char_data::getMoneyPtr()
+{
+    return &m_money;
+}
+
+void char_data::freeMoney()
+{
+    FREE(m_money);
+}
+
+void char_data::setMoney(char *value)
+{
+    m_money = value;
+}
+
+ubit8 char_data::getLastAttackerType() const
+{
+    return m_last_attacker_type;
+}
+
+void char_data::setLastAttackerType(ubit8 value)
+{
+    m_last_attacker_type = value;
+}
---
+++ vme/src/char_data.h
@@ -0,0 +1,195 @@
+#pragma once
+
+#include "char_follow_type.h"
+#include "char_point_data.h"
+#include "combat.h"
+#include "descriptor_data.h"
+#include "unit_data.h"
+
+class char_data : public unit_data
+{
+public:
+    explicit char_data(ubit8 unit_type);
+    virtual ~char_data();
+
+    /**
+     * @name Descriptor related code
+     * @{
+     */
+    const descriptor_data *getDescriptor() const;
+    descriptor_data *getDescriptor();
+    void setDescriptor(descriptor_data *value);
+    ///@}
+
+    /**
+     * @name Combat related code
+     * @{
+     */
+    const cCombat *getCombat() const;
+    cCombat *getCombat();
+    void setCombat(cCombat *value);
+    ///@}
+
+    /**
+     * @name Master related code
+     * @{
+     */
+    const unit_data *getMaster() const;
+    unit_data *getMaster();
+    unit_data **getMasterPtr();
+    void setMaster(unit_data *value);
+    ///@}
+
+    /**
+     * @name Last Location related code
+     * @{
+     */
+    const unit_data *getLastLocation() const;
+    unit_data *getLastLocation();
+    unit_data **getLastLocationPtr();
+    void setLastLocation(unit_data *value);
+    ///@}
+
+    /**
+     * @name Character points related functions
+     * @todo This is basically char_point_data's public interface - so in future char_point_data can be just removed
+     * @{
+     */
+    void readFrom(CByteBuffer &buf, ubit8 unit_version, unit_data *unit, int &error);
+
+    ubit32 *getCharacterFlagsPtr();
+    ubit32 getCharacterFlags() const;
+    void setAllCharacterFlags(ubit32 value);
+    void setCharacterFlag(ubit32 value);
+    void removeCharacterFlag(ubit32 value);
+    void toggleCharacterFlag(ubit32 value);
+
+    sbit32 getPlayerExperience() const;
+    sbit32 *getPlayerExperiencePtr();
+    void setPlayerExperience(sbit32 value);
+    void increasePlayerExperienceBy(sbit32 value);
+
+    ubit16 getRace() const;
+    ubit16 *getRacePtr();
+    void setRace(ubit16 value);
+
+    sbit16 getMana() const;
+    sbit16 *getManaPtr();
+    void setMana(sbit16 value);
+    void decreaseManaBy(sbit16 value);
+
+    sbit16 getEndurance() const;
+    sbit16 *getEndurancePtr();
+    void setEndurance(sbit16 value);
+    void decreaseEnduranceBy(sbit16 value);
+
+    sbit16 getOffensiveBonus() const;
+    void setOffensiveBonus(sbit16 value);
+    sbit16 *getOffensiveBonusPtr();
+
+    sbit16 getDefensiveBonus() const;
+    void setDefensiveBonus(sbit16 value);
+    sbit16 *getDefensiveBonusPtr();
+
+    int getNumberOfMeleeAttacks(int naturalAttacks);
+
+    sbit16 getSpeed() const;
+    void setSpeed(sbit16 value);
+    void modifySpeedBy(sbit16 delta);
+    /**
+     * Calculate the speed percentage. 200% max for players, 300% max for NPCs. 25% is the worst possible (4x slowed)
+     * @return speed percentage
+     */
+    int getSpeedPercentage();
+
+    ubit8 getNaturalArmor() const;
+    void setNaturalArmor(ubit8 value);
+    ubit8 *getNaturalArmorPtr();
+
+    ubit8 getAttackType() const;
+    void setAttackType(ubit8 value);
+    ubit8 *getAttackTypePtr();
+
+    ubit8 getSex() const;
+    void setSex(ubit8 value);
+    ubit8 *getSexPtr();
+
+    ubit8 getLevel() const;
+    void setLevel(ubit8 value);
+    ubit8 *getLevelPtr();
+    void incrementLevel();
+
+    ubit8 getPosition() const;
+    void setPosition(ubit8 value);
+    ubit8 *getPositionPtr();
+
+    sbit16 getAbilityAtIndex(size_t index) const;
+    sbit16 *getAbilityAtIndexPtr(size_t index);
+    sbit16 getSTR() const;
+    sbit16 getDEX() const;
+    sbit16 getCON() const;
+    sbit16 getCHA() const;
+    sbit16 getBRA() const;
+    sbit16 getMAG() const;
+    sbit16 getDIV() const;
+    sbit16 getHPP() const;
+    void setSTR(sbit16 value);
+    void setDEX(sbit16 value);
+    void setCON(sbit16 value);
+    void setCHA(sbit16 value);
+    void setBRA(sbit16 value);
+    void setMAG(sbit16 value);
+    void setDIV(sbit16 value);
+    void setHPP(sbit16 value);
+    void setAbilityAtIndexTo(size_t index, sbit16 value);
+    void increaseAbilityAtIndexBy(size_t index, sbit16 value);
+    void decreaseAbilityAtIndexBy(size_t index, sbit16 value);
+    std::array<sbit16, ABIL_TREE_MAX> &getAbilitiesArray();
+    /// @}
+
+    /**
+     * @name Follower related code
+     * @{
+     */
+    const char_follow_type *getFollowers() const;
+    char_follow_type *getFollowers();
+    void setFollowers(char_follow_type *value);
+    /// @}
+
+    /**
+     * @name
+     * @{
+     */
+    const char *getLastAttacker() const;
+    void freeLastAttacker();
+    void setLastAttacker(char *value);
+    ///@}
+
+    /**
+     * @name Money related functions
+     */
+    const char *getMoney() const;
+    char *getMoney();
+    char **getMoneyPtr();
+    void freeMoney();
+    void setMoney(char *value);
+    ///@}
+
+    /**
+     * @name Last Attacker type functions
+     * @{
+     */
+    ubit8 getLastAttackerType() const;
+    void setLastAttackerType(ubit8 value);
+    ///@}
+private:
+    descriptor_data *m_descriptor{nullptr}; ///<
+    cCombat *m_combat{nullptr};             ///<
+    unit_data *m_master{nullptr};           ///< Must be a char
+    unit_data *m_last_room{nullptr};        ///< Last location of character
+    char_point_data m_points;               ///<
+    char_follow_type *m_followers{nullptr}; ///<
+    char *m_last_attacker{nullptr};         ///< Last attacker of character
+    char *m_money{nullptr};                 ///<  Money transfer from db-files.
+    ubit8 m_last_attacker_type{0};          ///< Last attacker type of character
+};
